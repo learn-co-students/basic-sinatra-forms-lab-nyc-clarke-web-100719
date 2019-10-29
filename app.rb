108039b8bfd1af4c93bf274ceaa8d2aabaa518b0
soufianeou <soufiane.oucherrou@gmail.com>
2019-10-29 10:32:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,22 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+post "/team"  do
+    @all_info = params
+    @name = params[:name]
+    @coach =params[:coach]
+    @pg= params[:pg]
+    @sg =params[:sg]
+    @sf =params[:sf]
+    @pf =params[:pf]
+    @c =params[:c]
+erb :team
+#binding.pry
+end
+
+get "/newteam" do 
+    erb :newteam
+end
+
+
 end
